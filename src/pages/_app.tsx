c0fb0b48e1a5aa2f5d9ddd04f8a4bdd0c0d6d041
notnulldev <notnulldev0@gmail.com>
2023-05-11 03:27:00 +0200

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,29 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { clsx } from "clsx";
+import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+import Link from "next/link";
+import { Button } from "../../components/ui/button";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={(clsx(inter.className), "min-h-screen")}>
+      <Header />
+      <Component {...pageProps} />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="p-4 ">
+      <Button variant="ghost">
+        <Link href={"/"} className="text-white">
+          EasyAuth
+        </Link>
+      </Button>
+    </header>
+  );
 }
